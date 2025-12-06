1d879e0169fec8f59bdce39baec420a12fa32394
poliaavish <pvishnevska8@gmail.com>
2025-12-06 15:08:28 +0500

    BREAKING: introduce bug in test

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ from calculator import Calculator
 
 def test_add():
     calc = Calculator()
-    assert calc.add(2, 3) == 5
+    assert calc.add(2, 3) == 6
     assert calc.add(-1, 1) == 0
     assert calc.add(0, 0) == 0
 
